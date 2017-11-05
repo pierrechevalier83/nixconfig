9fc68e845cf082216616121534d15819209d7f7f
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-11-05 17:20:29 +0000

    Add mpv and transmission, try to improve zsh

--- configuration.nix
+++ configuration.nix
@@ -41,10 +41,13 @@
     git clang gcc
     zsh nix-zsh-completions
     numix-solarized-gtk-theme
+    mpv
+    transmission
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
+  programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
